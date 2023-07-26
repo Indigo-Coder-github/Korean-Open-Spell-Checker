1b57d877f4fec326b6be5d7413e33a0134cae9b3
Black-Stringer <hjs40111@gmail.com>
2023-07-26 16:21:54 +0900

    initialize python package

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="KOSC",
+    version="0.0.1",
+    description="This is open source project of Korean spell checker made by Python",
+    author="Indigo-Coder",
+    author_email="hjs40111@gmail.com",
+    url='https://github.com/Indigo-Coder-github/Korean-Open-Spell-Checker',
+    install_requires=[],
+    keywords=[],
+    python_requires='>=3.6'
+)
